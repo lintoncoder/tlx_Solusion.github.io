214bfbac9dec19d019e39ac449f5732dbe42bd45
Samuel I. H. Sibuea <87352987+lintoncoder@users.noreply.github.com>
2021-08-26 09:53:55 +0700

    Update Hello_World.cpp

--- Pemograman_Dasar/Part 1/Hello_World.cpp
+++ Pemograman_Dasar/Part 1/Hello_World.cpp
@@ -1,1 +1,10 @@
+#include <iostream> // Preprocessor directive that includes the content of the standard C++ header file iostream.
 
+using namespace std;
+/* This is a multiline comment
+second line comment */
+int main ()
+{
+    cout << "Halo, dunia!";
+    return 0;
+}
